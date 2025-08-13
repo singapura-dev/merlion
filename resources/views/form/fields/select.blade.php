bb89777ac96149ebd25ec62d58e051fb332af654
Alex <alex@parse.cn>
2025-08-13 21:55:24 +0800

    Add selected attribute to option based on field value

--- resources/views/form/fields/select.blade.php
+++ resources/views/form/fields/select.blade.php
@@ -2,6 +2,7 @@
     $name = $self->getName();
     $id = $self->getId();
     $label = $self->getLabel();
+    $value = $self->getValue();
     if(isset($errors) && $errors->has($name)) {
        $attributes = $attributes->merge(['class' => 'is-invalid']);
     }
@@ -11,7 +12,9 @@
 <x-merlion::form.field :$label :$id :$full :$label_position>
     <select name="{{$name}}" id="{{$id}}" {{$attributes->merge(['class' => 'form-select'])}}>
         @foreach($options as $option_key=> $option_label)
-            <option value="{{$option_key}}">{!! $option_label !!}</option>
+            <option value="{{$option_key}}"
+                    @if(old($name, $value) == $option_key) selected @endif
+            >{!! $option_label !!}</option>
         @endforeach
     </select>
 </x-merlion::form.field>
