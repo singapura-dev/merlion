f48c1fc3b81893902d04bb86daf39ec09a580177
Alex <alex@parse.cn>
2025-08-13 17:15:40 +0800

    Add Chinese translations for base actions

---
+++ lang/zh_CN/base.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'actions' => 'Actions',
+    'create' => 'Create',
+    'delete' => 'Delete',
+    'edit' => "Edit",
+    'logout' => 'Logout',
+    'record_deleted_at' => 'This record has been deleted, delete time :deleted_at',
+    'restore' => 'Restore',
+    'submit' => 'Submit',
+    'view' => 'View',
+];
