369042e6b46e6c4c2dd4c52f84d13cc207f70257
Alex <alex@parse.cn>
2025-08-14 22:28:58 +0800

    Add HasModal concern to Text column

--- src/Components/Table/Columns/Text.php
+++ src/Components/Table/Columns/Text.php
@@ -6,10 +6,12 @@ namespace Merlion\Components\Table\Columns;
 use Merlion\Components\Concerns\AsLink;
 use Merlion\Components\Concerns\Copyable;
 use Merlion\Components\Concerns\HasIcon;
+use Merlion\Components\Concerns\HasModal;
 
 class Text extends Column
 {
     use Copyable;
     use AsLink;
     use HasIcon;
+    use HasModal;
 }
