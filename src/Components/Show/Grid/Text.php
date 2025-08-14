c6d3de7eab331187045e18532ef4145b650e7212
Alex <alex@parse.cn>
2025-08-14 22:47:26 +0800

    Add AsLink trait to Text component

--- src/Components/Show/Grid/Text.php
+++ src/Components/Show/Grid/Text.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace Merlion\Components\Show\Grid;
 
+use Merlion\Components\Concerns\AsLink;
 use Merlion\Components\Concerns\Copyable;
 
 class Text extends Grid
 {
     use Copyable;
+    use AsLink;
 }
