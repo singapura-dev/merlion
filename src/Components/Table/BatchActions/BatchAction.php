906f6510f483b8811a5db46189d802ead5f3839b
Alex <alex@parse.cn>
2025-08-20 21:16:43 +0800

    Refactor BatchAction class to extend Button class

--- src/Components/Table/BatchActions/BatchAction.php
+++ src/Components/Table/BatchActions/BatchAction.php
@@ -3,53 +3,13 @@
 namespace Merlion\Components\Table\BatchActions;
 
 use Merlion\Components\Button;
-use Merlion\Components\Concerns\AsCell;
-use Merlion\Components\Concerns\HasIcon;
 use Merlion\Components\Containers\Container;
 use Merlion\Components\Form\Form;
-use Merlion\Components\Renderable;
 
-class BatchAction extends Renderable
+class BatchAction extends Button
 {
-    use AsCell;
-    use HasIcon;
-
-    protected ?Form $form = null;
-
-    public function handle()
-    {
-        // do something
-        return back();
-    }
-
     public function action($action, $method = 'post'): static
     {
         return $this->withAttributes(['data-batch-action' => $action, 'data-method' => $method]);
-    }
-
-    public function getForm()
-    {
-        return $this->form;
-    }
-
-    public function form($fields = []): static
-    {
-        if (empty($this->form)) {
-            $this->form = Form::make();
-            $this->form->content(
-                Container::make(Button::make('submit', 'Submit'), 'footer')->class('mt-3'),
-                'footer'
-            );
-        }
-
-        if (!empty($fields)) {
-            $this->form->fields($fields);
-        }
-
-        return $this;
-    }
-
-    public function confirm($title, $content = '', $type = 'danger')
-    {
     }
 }
