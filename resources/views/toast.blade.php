46fd6e05330d3bc6c8a0003e1991bbfb24f9fd00
Alex <alex@parse.cn>
2025-08-22 14:54:29 +0800

    Add showToast method to display toast messages

--- resources/views/toast.blade.php
+++ resources/views/toast.blade.php
@@ -1,17 +1,12 @@
 @pushonce('after_scripts')
-    <script nonce="{{csp_nonce()}}">
-        @if($message = admin()->getMessage())
-        (function () {
-            Toastify({
-                text: "{{$message}}",
-                duration: 3000,
-                position: "center",
-                style: {
-                    background: "unset",
-                },
-                className: "bg-{{session('toast.type')}}",
-            }).showToast();
-        })();
-        @endif
-    </script>
+    @if($message = admin()->getMessage())
+        <script nonce="{{csp_nonce()}}">
+            (function () {
+                admin().showToast({
+                    message: "{{$message}}",
+                    type: "{{session('toast.type')}}"
+                });
+            })();
+        </script>
+    @endif
 @endpushonce
