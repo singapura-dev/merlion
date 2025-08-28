9409bfc6b97f1bb978df75a837c314e3554a0bd9
Alex <alex@parse.cn>
2025-08-28 21:05:49 +0800

    Remove redundant icon position handling in HasIcon trait

--- src/Components/Concerns/HasIcon.php
+++ src/Components/Concerns/HasIcon.php
@@ -9,8 +9,6 @@ trait HasIcon
 {
     protected mixed $icon = null;
 
-    public mixed $iconPosition = null; // start | end
-
     public function icon(mixed $icon): static
     {
         $this->icon = $icon;
@@ -22,10 +20,6 @@ trait HasIcon
         if (empty($this->icon)) {
             return null;
         }
-        $icon = Icon::generate($this->evaluate($this->icon));
-        if (!empty($this->iconPosition)) {
-            $icon->position($this->evaluate($this->iconPosition));
-        }
-        return $icon;
+        return Icon::generate($this->evaluate($this->icon));
     }
 }
