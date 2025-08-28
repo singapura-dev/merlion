98424e7650498303e3046a1d7feda5c5144b77cf
Alex <alex@parse.cn>
2025-08-28 21:03:32 +0800

    Remove redundant icon rendering logic in text components

--- resources/views/show/grid/text.blade.php
+++ resources/views/show/grid/text.blade.php
@@ -1,8 +1,5 @@
 <x-merlion::datagrid :label="$self->getLabel()" :full="$self->getFull()">
     <x-merlion::text {{$attributes}} :icon="$self->getIcon()" :hasLink="$self->hasLink()">
-        @if($icon = $self->getIcon())
-            {!! render($icon) !!}
-        @endif
         {!! to_string($self->diaplayValue()) !!}
     </x-merlion::text>
 </x-merlion::datagrid>
--- resources/views/table/columns/text.blade.php
+++ resources/views/table/columns/text.blade.php
@@ -1,6 +1,3 @@
 <x-merlion::text {{$attributes}} :icon="$self->getIcon()" :hasLink="$self->hasLink()">
-    @if($icon = $self->getIcon())
-        {!! render($icon) !!}
-    @endif
     {!! to_string($self->diaplayValue()) !!}
 </x-merlion::text>
