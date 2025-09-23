647cb566a65cabe8c094441eb55815f19ce20b21
Alex <alex@parse.cn>
2025-09-23 11:21:45 +0800

    Update Table namespace and remove unnecessary imports

--- src/Http/Controllers/Concerns/HasIndex.php
+++ src/Http/Controllers/Concerns/HasIndex.php
@@ -9,7 +9,7 @@ use Merlion\Components\Table\BatchActions;
 use Merlion\Components\Table\Columns\Actions;
 use Merlion\Components\Table\Columns\Column;
 use Merlion\Components\Table\Filters;
-use tinker\packages\singapura\merlion\src\Components\Table\Table;
+use Merlion\Components\Table\Table;
 
 /**
  * @mixin AsCurdController
--- src/Http/Controllers/CrudController.php
+++ src/Http/Controllers/CrudController.php
@@ -5,9 +5,9 @@ namespace Merlion\Http\Controllers;
 
 use Merlion\Http\Controllers\Concerns\AsCurdController;
 use Merlion\Http\Controllers\Concerns\HasDestroy;
-use tinker\packages\singapura\merlion\src\Http\Controllers\Concerns\HasForm;
+use Merlion\Http\Controllers\Concerns\HasForm;
 use Merlion\Http\Controllers\Concerns\HasIndex;
-use tinker\packages\singapura\merlion\src\Http\Controllers\Concerns\HasShow;
+use Merlion\Http\Controllers\Concerns\HasShow;
 
 abstract class CrudController
 {
