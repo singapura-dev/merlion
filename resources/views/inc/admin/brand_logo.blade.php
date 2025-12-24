f5b0aea6728837e4f62a0b6666cf4f6a7ffbbd45
Alex <alex@parse.cn>
2025-12-24 13:16:14 +0800

    Refactor admin brand logo and header components

--- resources/views/inc/admin/brand_logo.blade.php
+++ resources/views/inc/admin/brand_logo.blade.php
@@ -1,9 +1,13 @@
-@if($brandLogo = admin()->getBrandLogo())
-    <img src="{{$brandLogo}}" alt="Brand logo">
-@else
-    @if($brandName = admin()->getBrandName())
-        {!! admin()->getBrandName() !!}
-    @else
-        <strong>Mer</strong>lion
-    @endif
-@endif
+<a class="nav-link" href="{{admin()->getRoute('home')}}">
+    <div class="brand-logo h-28px">
+        @if($brandLogo = admin()->getBrandLogo())
+            <img src="{{$brandLogo}}" alt="Brand logo">
+        @else
+            @if($brandName = admin()->getBrandName())
+                {!! admin()->getBrandName() !!}
+            @else
+                <strong>Mer</strong>lion
+            @endif
+        @endif
+    </div>
+</a>
--- resources/views/inc/admin/header.blade.php
+++ resources/views/inc/admin/header.blade.php
@@ -1,15 +1,11 @@
-<header class="navbar navbar-expand-md d-print-none">
+<header {{admin()->getAttributes('header')->merge(['class'=>"navbar navbar-expand-md d-print-none"])}}>
     <div class="container-xl">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu"
                 aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
-            <a class="nav-link" href="{{admin()->getRoute('home')}}">
-                <div class="brand-logo h-28px">
-                    @include(admin()->view('brand_logo'))
-                </div>
-            </a>
+            @include(admin()->view('brand_logo'))
         </div>
         {!! render(admin()->getContent('before_main_nav')) !!}
         @include(admin()->view('user_nav'))
