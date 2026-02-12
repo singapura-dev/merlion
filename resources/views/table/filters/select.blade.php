59d29e4080eefb1800ba8e8f2d9b631402b0e441
Alex <alex@parse.cn>
2026-02-12 12:03:00 +0800

    Refactor select filter to support multiple selections

--- resources/views/table/filters/select.blade.php
+++ resources/views/table/filters/select.blade.php
@@ -2,16 +2,49 @@
     $value = $self->getValue();
 @endphp
 <x-merlion::form.field :label="$self->getLabel()" :$id :label_position="$labelPosition">
-    <select {{$attributes->merge(['class' => 'form-select'])}}
-            name="{{$self->getFilterName()}}">
-        <option value="">{{$self->getPlaceholder() ?: '-'}}</option>
+    <select {{$attributes->merge(['class' => 'form-select tomselected'])}}
+            @if($self->getMultiple())
+                multiple
+            name="{{$self->getFilterName()}}[]"
+            @else
+                name="{{$self->getFilterName()}}"
+        @endif
+    >
+        <option value="">{{$self->getPlaceholder() ?: ''}}</option>
         @foreach($options as $option_key => $option_label)
             <option
-                @if(!is_null($value))
-                    {{$value == $option_key ? 'selected' : ''}}
+                @if($self->getMultiple())
+                    @if(in_array($option_key, $value?:[])) selected @endif
+                @else
+                    @if(!is_null($value))
+                        {{$value == $option_key ? 'selected' : ''}}
+                    @endif
                 @endif
                 value="{{$option_key}}">{!! $option_label !!}</option>
         @endforeach
     </select>
+    @pushonce('before_styles')
+        <link rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/tom-select@2.5.1/dist/css/tom-select.bootstrap5.min.css">
+    @endpushonce
+    @pushonce('after_scripts')
+        <script src="https://cdn.jsdelivr.net/npm/tom-select@2.5.1/dist/js/tom-select.complete.min.js"></script>
+    @endpushonce
+    @push('scripts')
+        <script nonce="{{csp_nonce()}}">
+            document.addEventListener('DOMContentLoaded', function() {
+                new TomSelect('select#{{$self->getId()}}', {
+                    plugins: {
+                        remove_button: {
+                            title: 'Remove this item',
+                        },
+                        'clear_button': {
+                            'title': 'Remove all selected options',
+                        }
+                    },
+                });
+            });
+        </script>
+    @endpush
 </x-merlion::form.field>
 
--- src/Components/Table/Filters/Select.php
+++ src/Components/Table/Filters/Select.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Merlion\Components\Table\Filters;
 
+/**
+ * @method static options($options)
+ */
 class Select extends Text
 {
+    public bool $multiple = false;
+    public mixed $options = [];
 
+    public function multiple(bool $value = true): static
+    {
+        $this->multiple = $value;
+        return $this;
+    }
 }
